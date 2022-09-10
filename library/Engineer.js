2773f098682b72ecc1e9ad74a733ef7e97e01cff
Kevin Korza <107637260+KevinGKorza@users.noreply.github.com>
2022-09-09 21:54:56 -0400

    Update Engineer.js

--- library/Engineer.js
+++ library/Engineer.js
@@ -1,1 +1,20 @@
+//the importation from the employee constructor
+const Employee = require("./Employee");
 
+//Creation of engineer class that includes the name, id, and email
+//Creation of constructor
+
+class Engineer extends Employee {
+    constructor (name, id, email, github) {
+        Employee (name, id, email);
+        this.github = github;
+    }
+    getGithub(){
+        return this.github;
+    }
+    getRole() {
+        return "Engineer";
+    }
+
+}
+module.exports = Engineer;
