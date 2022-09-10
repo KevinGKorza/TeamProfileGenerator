0818e58349bb58f18554a1a77bff806a39594841
Kevin Korza <107637260+KevinGKorza@users.noreply.github.com>
2022-09-09 21:52:19 -0400

    Update Engineer.test.js

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,1 +1,20 @@
+//using Engineer constructor
+const Engineer = require("../library/Engineer");
 
+//creating onject for engineer
+test("an object for your engineer has been created", () => {
+    const engineer = new Engineer("Lenny Leonard", 3, "LLeonard@springfieldplant.com");
+    expect(engineer.github).toEqual(expect.any(String));
+});
+
+//gets github from getGitHub()
+test("your engineer github", () => {
+    const engineer = new Engineer("Lenny Leonard", 3, "LLeonard@springfieldplant.com", "LennyIsotopes#1");
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+
+//gets role from get Role()
+test("this is your role", () => {
+    const engineer = new Engineer("Lenny Leonard", 3, "LLeonard@springfieldplant.com", "LennyIsotopes#1");
+    expect(engineer.getRole()).toEqual("Engineer");
+});
