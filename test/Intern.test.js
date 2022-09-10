f1c9c3cdfe1e36431f8a4427fd20b17eb757dd34
Kevin Korza <107637260+KevinGKorza@users.noreply.github.com>
2022-09-09 21:52:47 -0400

    Update Intern.test.js

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -1,1 +1,20 @@
+//Intern constructor
+const Intern = require('../library/Intern');
 
+//Intern object
+test("an object has been created for Intern", () => {
+    const intern = new Intern("Frank Grimes", 4, "FGrimes@springfieldplant.com", "University of Springfield");
+    expect(intern.school).toEqual(expect.any(String));
+});
+
+//gets school from getSchool()
+test("gets intern school", () => {
+    const intern = new Intern("Frank Grimes", 4, "FGrimes@springfieldplant.com", "University of Springfield");
+    expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
+});
+
+//gets role from getRole()
+test("gets role of the intern", () => {
+    const intern = new Intern("Frank Grimes", 4, "FGrimes@springfieldplant.com", "University of Springfield");
+    expect(intern.getRole()).toEqual("Intern");
+});
