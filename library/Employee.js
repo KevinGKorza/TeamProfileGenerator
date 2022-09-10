09ac8e14b727882b8d3deccc0d5ef5f76b451533
Kevin Korza <107637260+KevinGKorza@users.noreply.github.com>
2022-09-09 21:54:27 -0400

    Update Employee.js

--- library/Employee.js
+++ library/Employee.js
@@ -1,1 +1,24 @@
+//Creation of employee class that includes the name, id, and email
+//Creation of constructor
 
+class Employee {
+    constructor (name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    getName(){
+        return this.name;
+    }
+    getId(){
+        return this.name;
+    }
+    getEmail(){
+        return this.email;
+    }
+    getRole(){
+        return 'Employee';
+    }
+};
+
+module.exports = Employee
