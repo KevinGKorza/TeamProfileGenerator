21e7c8ea6cdfb46c9a8a36b7b4088fdfe08edff3
Kevin Korza <107637260+KevinGKorza@users.noreply.github.com>
2022-09-09 21:51:46 -0400

    Update Employee.test.js

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,1 +1,33 @@
+const Employee = require('../library/Employee');
+//an employee has been created
+test('employee object',() => {
+    const employee = new Employee("Homer Simpson", 1, "hsimpson@springfieldplant.com");
+    expect(employee.email).toEqual(expect.any(String));
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+});
+
+test('employee name',() => {
+    const employee = new Employee("Homer Simpson", 1, "hsimpson@springfieldplant.com");
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+//gets id 
+test('employee ID', () => {
+    const employee = new Employee("Homer Simpson", 1, "hsimpson@springfieldplant.com");
+    expect(employee.getId()).toEqual(expect.any(String));
+});
+
+
+//gets email
+test('employee email', () => {
+    const employee = new Employee("Homer Simpson", 1, "hsimpson@springfieldplant.com");
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+});
+
+//gets role 
+test('get role', () => {
+    const employee = new Employee("Homer Simpson", 1, "hsimpson@springfieldplant.com");
+    expect(employee.getRole()).toEqual("Employee");
+});
 
