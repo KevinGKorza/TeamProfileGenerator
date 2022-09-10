713f349d2ffc35e1b7f134af17403426d6b677f8
Kevin Korza <107637260+KevinGKorza@users.noreply.github.com>
2022-09-09 21:53:14 -0400

    Update Manager.test.js

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -1,1 +1,13 @@
+//Manager constructor
+const Manager = require('../library/Manager');
+//manager object
+test("your manager has been created", () => {
+    const manager = new Manager("Carl Carlson", 2, "CCarlson@springfieldplant.com", 26);
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+});
 
+//gets role from getRole()
+test("your role of employee", () => {
+    const manager = new Manager("Carl Carlson", 2, "CCarlson@springfieldplant.com");
+    expect(manager.getRole()).toEqual("Manager");
+});
