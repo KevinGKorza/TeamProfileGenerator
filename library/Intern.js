0937825f1cb8114e3b9413b592f61df4b537ef8d
Kevin Korza <107637260+KevinGKorza@users.noreply.github.com>
2022-09-09 21:55:20 -0400

    Update Intern.js

--- library/Intern.js
+++ library/Intern.js
@@ -1,1 +1,14 @@
-
+const Employee = require('./Employee');
+class Intern extends Employee {
+    constructor (name, id, email, school) {
+        Employee (name, id, email);
+        this.school = school;
+    }
+    getSchool () {
+        return this.school;
+    }
+    getRole(){
+        return "Intern";
+    }
+}
+module.exports = Intern
