ebb44bb7349d4f1a8ee9fbe0698b11e1fe2c9847
Kevin Korza <107637260+KevinGKorza@users.noreply.github.com>
2022-09-12 13:31:35 -0400

    Update Employee.test.js

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,12 +1,11 @@
-//
 const Employee = require('../library/Employee');
 describe("Employee", () => {
     describe("Initialization", () => {
         it("should return an object with the name of, id of, and email of employee", () => {
-            const Employee = new Employee("Homer Simpson", 1, "HSimpson@gmail.com");
-            expect (Employee.name).toEqual("Homer Simpson");
-            expect(Employee.id).toEqual(1);
-            expect(Employee.email).toEqual("HSimpson@gmail.com");
+            const employee = new employee("Homer Simpson", 1, "HSimpson@gmail.com");
+            expect (employee.name).toEqual("Homer Simpson");
+            expect(employee.id).toEqual(1);
+            expect(employee.email).toEqual("HSimpson@gmail.com");
         });
     });
     //the name of the employee
@@ -19,22 +18,22 @@ describe("Employee", () => {
     //the id
     describe("getId", () => {
         it("should return the ID of the Employee", () => {
-            const Employee = new Employee("Homer Simpson", 1, "HSimpson@gmail.com");
-            expect(Employee.getID()).toEqual(1);
+            const employee = new employee("Homer Simpson", 1, "HSimpson@gmail.com");
+            expect(employee.getID()).toEqual(1);
         });
     });
     //the email
     describe("getEmail", () => {
         it("should return the email of the Employee", () => {
-            const Employee = new Employee("Homer Simpson", 1, "HSimpson@gmail.com");
-            expect(Employee.getEmail()).toEqual("HSimpson@gmail.com");
+            const employee = new employee("Homer Simpson", 1, "HSimpson@gmail.com");
+            expect(employee.getEmail()).toEqual("HSimpson@gmail.com");
         });
     });
     //the role
     describe("getRole", () => {
         it("should return the role of the Employee", () => {
-            const Employee = new Employee("Homer Simpson", 1, "HSimpson@gmail.com");
-            expect(Employee.getRole()).toEqual("Employee");
+            const employee = new employee("Homer Simpson", 1, "HSimpson@gmail.com");
+            expect(employee.getRole()).toEqual("Employee");
         });
     });
 });
