2492e15dccba5473a2d032a8631c6bce6ba6488a
Kevin Korza <107637260+KevinGKorza@users.noreply.github.com>
2022-09-12 14:05:16 -0400

    Update Engineer.test.js

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,20 +1,28 @@
 //using Engineer constructor
 const Engineer = require("../library/Engineer");
+describe("Engineer", () => {
+    describe("Initialization", () => {
+        it("should return an object with the name of, id of, email of, and github account of the engineer", () => {
+            const engineer = new Engineer("Lenny Leonard", 4, "LLeonard@gmail.com", "LennyIzKool");
+            expect(engineer.name).toEqual("Lenny Leonard");
+            expect(engineer.id).toEqual(4);
+            expect(engineer.email).toEqual("LLeonard@gmail.com");
+            expect(engineer.github).toEqual("LennyIzKool");
+        });
+    });
+    //github
+    describe("getGithub", () => {
+        it("should return the Engineer's GitHub account", () => {
+            const engineer = new Engineer("Lenny Leonard", 4, "LLeonard@gmail.com", "LennyIzKool");
+            expect(engineer.getGithub()).toEqual("LennyIzKool");
+        });
+    });
+    //the role
+    describe("getRole", () => {
+        it("should return the Engineer's role", () => {
+            const engineer = new Engineer("Lenny Leonard", 4, "LLeonard@gmail.com", "LennyIzKool");
+            expect(engineer.getRole()).toEqual("Engineer");
+        });
+    });
 
-//creating onject for engineer
-test("an object for your engineer has been created", () => {
-    const engineer = new Engineer("Lenny Leonard", 3, "LLeonard@springfieldplant.com");
-    expect(engineer.github).toEqual(expect.any(String));
-});
-
-//gets github from getGitHub()
-test("your engineer github", () => {
-    const engineer = new Engineer("Lenny Leonard", 3, "LLeonard@springfieldplant.com", "LennyIsotopes#1");
-    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
-});
-
-//gets role from get Role()
-test("this is your role", () => {
-    const engineer = new Engineer("Lenny Leonard", 3, "LLeonard@springfieldplant.com", "LennyIsotopes#1");
-    expect(engineer.getRole()).toEqual("Engineer");
 });
