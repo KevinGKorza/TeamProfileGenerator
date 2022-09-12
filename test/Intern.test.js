5df6df3860fbcc97bb64b60c80e843394098798a
Kevin Korza <107637260+KevinGKorza@users.noreply.github.com>
2022-09-12 14:25:17 -0400

    Update Intern.test.js

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -1,1 +1,14 @@
-
+//Intern constructor
+const Intern = require('../library/Intern');
+describe("Intern", () => {
+    describe("Initialization", () => {
+        it("should return an object with the name of, id of, email of, and University of the Intern", ()=> {
+            const intern = new Intern("Frank Grimes", 0, "FGrimes@gmail.com", "Springfield U");
+            expect(intern.name).toEqual("Frank Grimes");
+            expect(intern.id).toEqual(0);
+            expect(intern.email).toEqual("FGrimes@gmail.com");
+            expect(intern.school).toEqual("Springfield U");
+        });
+    });
+    
+})
